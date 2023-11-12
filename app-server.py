2a17bd53ae701cd4a874307d6d0cce274c005712
Martin Kitonga <119602263+blacksnowmartin@users.noreply.github.com>
2023-11-12 14:17:36 +0300

    Create app-server.py
    Used to get a reference back to the message object using the data attribute on the key object and call a method in Message:

---
+++ app-server.py
@@ -0,0 +1,17 @@
+# app-server.py
+
+# ...
+
+try:
+    while True:
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                message = key.data
+                try:
+                    message.process_events(mask)
+                # ...
+
+# ...
